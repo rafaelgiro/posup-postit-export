8b5cd6662fd8884e694ee8048f03f7701b6bf51a
Sabrina Travasso Ferreira <sabrinatravasso@hotmail.com>
2022-04-19 18:39:13 -0300

    Config banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostItModule } from './postit/postit.module';
@@ -5,7 +6,7 @@ import { PostItModule } from './postit/postit.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://userpostit:userpostit123@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=30&appName=mongosh+1.3.1',
+      'mongodb://userpostit:userpostit123@10.103.190.209:27017/test?directConnection=true&serverSelectionTimeoutMS=30&appName=mongosh+1.3.1',
     ),
     PostItModule,
   ],
